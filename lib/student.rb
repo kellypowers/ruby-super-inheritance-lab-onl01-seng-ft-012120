ed1fee05f19e207ccf2bd07f964447a96e292300
kellypowers <kpowers.nj@gmail.com>
2020-01-29 17:06:30 -0500

    passes tests

---
+++ lib/student.rb
@@ -0,0 +1,17 @@
+class Student 
+    attr_accessor :greeting
+
+    def raise_hand
+        phrase = "Pick me!"
+        puts phrase
+    end
+
+    def hello
+        greeting = "Hey there! I'm so excited to learn stuff.\n"
+        puts greeting
+        greeting
+    end
+
+
+
+end
